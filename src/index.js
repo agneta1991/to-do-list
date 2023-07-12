48f9e0fba177aeacc2ecf6cb99413d7dcb1d0d6c
agneta1991 <pupienytea@gmail.com>
2023-07-12 12:16:12 +0300

    added style

--- src/index.js
+++ src/index.js
@@ -1,59 +1,44 @@
 import './style.css';
 
 const taskInput = document.getElementById('input');
-const tasks = [];
+const tasks = [
+    {
+        description: 'task1',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'task2',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'task3',
+        completed: false,
+        index: 2,
+    },
+];
 
-function addTasks() {
-  const taskList = document.getElementById('taskList');
-  taskList.innerHTML = '';
+const taskList = document.getElementById('taskList');
 
-  tasks.forEach((task) => {
+tasks.forEach((task) => {
     const listItem = document.createElement('li');
     listItem.className = 'list';
-
-    const insideList = document.createElement('label');
-    insideList.className = 'listItemsLabel';
-    insideList.for = 'myCheck';
-    insideList.innerHTML = task.description;
+    taskList.appendChild(listItem);
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.className = 'checkboxtick';
-
-    checkbox.addEventListener('change', () => {
-      if (checkbox.checked) {
-        insideList.classList.add('completed');
-      } else {
-        insideList.classList.remove('completed');
-      }
-    });
-
-    if (task.completed) {
-      checkbox.checked = true;
-      insideList.classList.add('completed');
-    }
-
     listItem.appendChild(checkbox);
-    listItem.appendChild(insideList);
-    taskList.appendChild(listItem);
-  });
-}
 
-taskInput.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    const description = taskInput.value;
+    const p = document.createElement('p');
+    p.innerHTML = task.description;
 
-    const newTask = {
-      description,
-      completed: false,
-      index: tasks.length + 1,
-    };
+    listItem.appendChild(p);
 
-    tasks.push(newTask);
-    taskInput.value = '';
-    addTasks();
-  }
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-ellipsis-vertical';
+    listItem.appendChild(icon);
+
 });
 
-document.addEventListener('DOMContentLoaded', addTasks);
