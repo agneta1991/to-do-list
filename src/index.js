19d6d6f32607131b88dc3e8169a57ece8781b038
agneta1991 <pupienytea@gmail.com>
2023-07-12 12:23:15 +0300

    linters fixed

--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
 import './style.css';
 
-const taskInput = document.getElementById('input');
 const tasks = [
-    {
-        description: 'task1',
-        completed: false,
-        index: 0,
-    },
-    {
-        description: 'task2',
-        completed: false,
-        index: 1,
-    },
-    {
-        description: 'task3',
-        completed: false,
-        index: 2,
-    },
+  {
+    description: 'task1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'task2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 2,
+  },
 ];
 
 const taskList = document.getElementById('taskList');
 
 tasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.className = 'list';
-    taskList.appendChild(listItem);
+  const listItem = document.createElement('li');
+  listItem.className = 'list';
+  taskList.appendChild(listItem);
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkboxtick';
-    listItem.appendChild(checkbox);
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'checkboxtick';
+  listItem.appendChild(checkbox);
 
-    const p = document.createElement('p');
-    p.innerHTML = task.description;
+  const p = document.createElement('p');
+  p.innerHTML = task.description;
 
-    listItem.appendChild(p);
+  listItem.appendChild(p);
 
-    const icon = document.createElement('i');
-    icon.className = 'fa-solid fa-ellipsis-vertical';
-    listItem.appendChild(icon);
-
+  const icon = document.createElement('i');
+  icon.className = 'fa-solid fa-ellipsis-vertical';
+  listItem.appendChild(icon);
 });
-
