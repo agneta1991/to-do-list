3a0d3a925aa649aaf9b412739c4e468bb22cb364
agneta1991 <pupienytea@gmail.com>
2023-07-12 14:26:47 +0300

    added editing functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'task1',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'task2',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'task3',
-    completed: false,
-    index: 2,
-  },
-];
+const tasks = [];
 
 const taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('input');
 
-tasks.forEach((task) => {
+function addTask(description) {
   const listItem = document.createElement('li');
   listItem.className = 'list';
   taskList.appendChild(listItem);
@@ -31,11 +16,67 @@ tasks.forEach((task) => {
   listItem.appendChild(checkbox);
 
   const p = document.createElement('p');
-  p.innerHTML = task.description;
-
+  p.innerHTML = description;
+  p.className = 'content';
   listItem.appendChild(p);
 
   const icon = document.createElement('i');
   icon.className = 'fa-solid fa-ellipsis-vertical';
   listItem.appendChild(icon);
+    icon.addEventListener('click', editFunction);
+
+    function editFunction(e) {
+        const clickedBtn = e.target;
+        const parent = clickedBtn.parentNode;
+        const content = parent.querySelector('.content');
+        const newContent = prompt('Edit Task');
+        if (newContent !== null) {
+          content.innerHTML = newContent;
+        }
+      }
+    }
+
+function addTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((task) => {
+    addTask(task.description);
+  });
+}
+
+taskInput.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    const description = taskInput.value.trim();
+
+    if (description !== '') {
+      const newTask = {
+        description: description,
+        completed: false,
+        index: tasks.length + 1,
+      };
+
+      tasks.push(newTask);
+      taskInput.value = '';
+      addTask(description);
+    }
+  }
+});
+
+
+
+/*function removeTask() {
+  const dotIcon = document.querySelectorAll('.fa-ellipsis-vertical');
+  dotIcon.forEach((individualDots) => {
+    individualDots.addEventListener('click', removeListItem);
+  });
+
+  function removeListItem(event) {
+    alert('yay');
+    /*const listItem = event.target.parentNode;
+    listItem.remove();
+  }
+}*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  addTasks();
 });
