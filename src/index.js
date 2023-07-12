bd05938cdc50d9f33404066b78ad32b636811a0e
agneta1991 <pupienytea@gmail.com>
2023-07-12 09:45:28 +0300

    linters

--- src/index.js
+++ src/index.js
@@ -3,63 +3,57 @@ import './style.css';
 const taskInput = document.getElementById('input');
 const tasks = [];
 
-taskInput.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        const description = taskInput.value;
-
-        const newTask = {
-            description: description,
-            completed: false,
-            index: tasks.length + 1,
-        };
-
-        tasks.push(newTask);
-        taskInput.value = '';
-        addTasks();
-
-    }
-})
-
-
 function addTasks() {
-    const taskList = document.getElementById('taskList');
-    taskList.innerHTML = '';
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
 
-    tasks.forEach((task) => {
-        const listItem = document.createElement('li');
-        listItem.className = 'list';
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'list';
 
-        const insideList = document.createElement('label');
-        insideList.className = 'listItemsLabel';
-        insideList.for = 'myCheck';
-        insideList.innerHTML = task.description;
+    const insideList = document.createElement('label');
+    insideList.className = 'listItemsLabel';
+    insideList.for = 'myCheck';
+    insideList.innerHTML = task.description;
 
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.className = 'checkboxtick';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkboxtick';
 
-
-        checkbox.addEventListener('change', function () {
-            if (checkbox.checked) {
-                insideList.classList.add('completed');
-            } else {
-                insideList.classList.remove('completed');
-            }
-        });
-
-        if (task.completed) {
-            checkbox.checked = true;
-            insideList.classList.add('completed');
-          }
-
-        listItem.appendChild(checkbox);
-        listItem.appendChild(insideList);
-        taskList.appendChild(listItem);
-
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        insideList.classList.add('completed');
+      } else {
+        insideList.classList.remove('completed');
+      }
     });
+
+    if (task.completed) {
+      checkbox.checked = true;
+      insideList.classList.add('completed');
+    }
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(insideList);
+    taskList.appendChild(listItem);
+  });
 }
 
+taskInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    const description = taskInput.value;
 
+    const newTask = {
+      description,
+      completed: false,
+      index: tasks.length + 1,
+    };
+
+    tasks.push(newTask);
+    taskInput.value = '';
+    addTasks();
+  }
+});
 
 document.addEventListener('DOMContentLoaded', addTasks);
